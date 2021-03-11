056c65856811151d05e66d57e6214c11ed17c300
Irene-zou <69069854+Irene-zou@users.noreply.github.com>
2021-03-10 18:58:39 -0600

    update

--- UFO-Level 1/static/js/app.js
+++ UFO-Level 1/static/js/app.js
@@ -1,42 +1,30 @@
-// from data.js
-var tableData = data;
+var tableData = data; // from data.js
+var button = d3.select("#filter-btn"); //select HTML element
+var form = d3.select("#form"); //select HTML element
 
-// YOUR CODE HERE!
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+function runEnter() {
+    d3.event.preventDefault(); // Prevent the page from refreshing
+    var inputElement = d3.select("#datetime"); // Select the input element and get the raw HTML node
+    var inputValue = inputElement.property("value"); // Get the value property of the input element
+    //filter the original array to only include dates which match the date value that the user inputs
+    var filteredData = [];
+    filteredData = tableData.filter(UFO => UFO.datetime === inputValue);
 
-tbody = d3.select("tbody")
-console.log("hello")
+    console.log(inputValue);
+    console.log(filteredData);
 
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
-
-  function displayData(data){ 
-    tbody.text("")
-    data.forEach(function(sighting){
-    new_tr = tbody.append("tr")
-    Object.entries(sighting).forEach(function([key, value]){
-        new_td = new_tr.append("td").text(value)	
-    })
-})}
-
-displayData(tableData)
-
-
-//Select the filter button
-var dateInputText = d3.select("#datetime")
-var button = d3.select("filter-btn")
-
-// Filter data
-function clickSelect(){
-    d3.event.preventDefault();
-    //Print the value
-    console.log(dateInputText.property("value"));
-    //New table showing only filtered data
-    var new_table = tableData.filter(sighting => sighting.datetime===dateInputText.property("value"))
-    displayData(new_table);
-}
-
-
-dateInputText.on("change", clickSelect)
+    var table = d3.select("tbody"); // Select the table element
+    //clear the table before appending new results to it
+    table.html("");
+    //add data to the table
+    filteredData.forEach((i) => {
+        var row = table.append("tr");
+        Object.entries(i).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+  };
